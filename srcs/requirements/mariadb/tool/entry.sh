44202882aecbbfaf38f45dbb4f3d93756677ab6d
gvial15 <guillaumevial15@gmail.com>
2023-10-11 19:33:14 -0400

    wordpress and mariadb working, nginx config not working

--- srcs/requirements/mariadb/tool/entry.sh
+++ srcs/requirements/mariadb/tool/entry.sh
@@ -3,28 +3,19 @@
 set -euo pipefail
 
 if [ "$1" = 'mysqld' ]; then
-    # Set the datadir
-    sed -Ei "/^datadir/c datadir                 = $MYSQL_DATADIR" /etc/mysql/mariadb.conf.d/my.cnf
-
     if [ ! -f "$MYSQL_DATADIR/initdb.sql" ]; then
-        chown -R mysql:mysql $MYSQL_DATADIR
-
 		echo mysql_install_db
-        mysql_install_db --datadir=$MYSQL_DATADIR --user=root --rpm --skip-test-db > /dev/null
+        mysql_install_db --datadir=$MYSQL_DATADIR --user=$MYSQL_USER --rpm --skip-test-db > /dev/null
 
-        # Create SQL script
         cat >"$MYSQL_DATADIR/initdb.sql" <<EOF
 CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
 GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
-FLUSH PRIVILEGES;
 
-ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 EOF
 
-        # Setup database
         mysqld --skip-networking=1 &
         for i in {0..30}; do
             if mysql -u root -proot --database=mysql <<<'SELECT 1;' &> /dev/null; then
@@ -35,7 +26,7 @@ EOF
         if [ "$i" = 30 ]; then
             echo "Error while starting server"
         fi
-        mysql -u root -proot < "$MYSQL_DATADIR/initdb.sql" && killall mysqld
+        mysql -u root -p$MYSQL_PASSWORD < "$MYSQL_DATADIR/initdb.sql" && killall mysqld
     fi
 
     echo "MariaDB listening on port 3306"
--- srcs/requirements/wordpress/tool/entry.sh
+++ srcs/requirements/wordpress/tool/entry.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+for i in {0..10}; do
+	if mariadb -h$MYSQL_SERVER -u$MYSQL_USER -p$MYSQL_PASSWORD --database=$MYSQL_DATABASE <<<'SELECT 1;' &>/dev/null; then
+		break
+	fi
+	sleep 2
+done
+if [ "$i" = 10 ]; then
+	echo "Can't connect to db"
+fi
 
 if [ ! -f "/var/www/html/wp-config.php" ]; then
+        tar -xzf wordpress-6.0.tar.gz
+        rm wordpress-6.0.tar.gz
+        mv wordpress//* var/www/html/
+        rm -rf wordpress
+
 		echo connecting to database...
 		mariadb -h$MYSQL_SERVER -u$MYSQL_USER -p$MYSQL_PASSWORD --database=$MYSQL_DATABASE
 		echo config create
@@ -16,18 +30,18 @@ if [ ! -f "/var/www/html/wp-config.php" ]; then
 		echo install
         wp core install --allow-root \
             --title="Wordpress" \
-            --admin_name="${MYSQL_USER}" \
-            --admin_password="${MYSQL_PASSWORD}" \
-            --admin_email="${MYSQL_EMAIL}" \
+            --admin_name=$MYSQL_USER \
+            --admin_password=$MYSQL_PASSWORD \
+            --admin_email=$MYSQL_EMAIL \
             --skip-email \
-            --url="${DOMAIN_NAME}" \
+            --url=$DOMAIN_NAME \
             --path="/var/www/html"
 		echo user create
         wp user create --allow-root \
             $WP_USER \
             $WP_EMAIL \
             --role=author \
-            --user_pass=$WP_PWD \
+            --user_pass=$WP_PASSWORD \
             --path="/var/www/html"
 fi
 
